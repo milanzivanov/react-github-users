015837d88578344c088cd90d84e1a85142b81e4a
milan.zivanov <milan.zivanov@gmail.com>
2021-08-13 15:27:23 +0200

    Send the data to the App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import Form from "./components/Form";
+import { useState } from 'react';
 
 function App() {
+
+  const [cards, setCards] = useState([]);
+
+  const addNewCard = (card) => {
+    setCards([...cards, card]);
+  }
+
   return (
     <div>
       <h1 className="pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900">
         Search a GitHub User
       </h1>
 
-      <Form></Form>
+      <Form onSubmit={addNewCard}></Form>
     </div>
   );
 }
