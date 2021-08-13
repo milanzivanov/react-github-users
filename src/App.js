387c3d26cf0c97344f75097fbe6c57e980640b59
milan.zivanov <milan.zivanov@gmail.com>
2021-08-13 15:55:44 +0200

    Displaying cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import React, { useState } from 'react';
 import './App.css';
-import Form from "./components/Form";
-import { useState } from 'react';
+import Form from './components/Form';
+import CardList from './components/CardList';
 
 function App() {
-
   const [cards, setCards] = useState([]);
 
   const addNewCard = (card) => {
@@ -12,13 +12,13 @@ function App() {
 
   return (
     <div>
-      <h1 className="pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900">
+      <h1 className='pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900'>
         Search a GitHub User
       </h1>
-
-      <Form onSubmit={addNewCard}></Form>
+      <Form onSubmit={addNewCard} />
+      <CardList cards={cards} />
     </div>
-  );
+  )
 }
 
 export default App;
