5b4e560bfc6d3641171858cc4fe2efb7c2bb6e92
milan.zivanov <milan.zivanov@gmail.com>
2021-09-07 17:15:10 +0200

    some updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 import './App.css';
 import Form from './components/Form';
 import CardList from './components/CardList';
